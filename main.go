4a75d44246a7b868dd3c7b4ec23b1a03a54ec2f9
sungchul-dev <devnori@bigin.io>
2020-03-12 16:41:51 +0900

    [ADD] Account 패키지 추가

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Sungchul-P/learngo/accounts"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	account := accounts.NewAccount("devnori")
+	account.Deposit(10)
+	account.ChangeOwner("ch-devnori")
+	// fmt.Println(account.Balance())
+	// err := account.Withdraw(20)
+	// if err != nil {
+	// 	// log.Fatalln(err)
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(account.Balance(), account.Owner())
+	fmt.Println(account)
 }
